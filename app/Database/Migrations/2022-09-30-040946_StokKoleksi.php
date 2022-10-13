195bdd3694d104293cf17368b84f43c66b5f7026
SyarifahRaihani-12200537 <113585321+SyarifahRaihani-12200537@users.noreply.github.com>
2022-10-13 21:47:29 +0700

    Update 2022-09-30-040946_StokKoleksi.php
    versi 1.02

--- app/Database/Migrations/2022-09-30-040946_StokKoleksi.php
+++ app/Database/Migrations/2022-09-30-040946_StokKoleksi.php
@@ -11,18 +11,18 @@ class StokKoleksi extends Migration
         $this->forge->addField([
             'id'                =>[ 'type' => 'int', 'constraint' => 10, 'unsigned'=>true, 'auto_increment'=>true ],
             'koleksi_id'        =>[ 'type' => 'int', 'constraint' => 10, ],
-            'nomor'             =>[ 'type' => 'int', 'constraint' => 10, 'default' => '1' ],
+            'nomor'             =>[ 'type' => 'int', 'constraint' => 10, ],
             'status_tersedia'   =>[ 'type' => 'enum("A", "P", "R", "H")', 'default' => 'A' ],
-            'anggota_id'        =>[ 'type' => 'int', 'constraint' => 10, 'unsigned'=>true, 'auto_increment'=>true ],
-            'pustakawan_id'     =>[ 'type' => 'int', 'constraint' => 10, 'unsigned'=>true, 'auto_increment'=>true ],
+            'anggota_id'        =>[ 'type' => 'int', 'constraint' => 10, 'unsigned'=>true ],
+            'pustakawan_id'     =>[ 'type' => 'int', 'constraint' => 10, 'unsigned'=>true ],
             'created_at'        =>[ 'type' => 'datetime', 'null'=>true],
             'updated_at'        =>[ 'type' => 'datetime', 'null'=>true],
             'deleted_at'        =>[ 'type' => 'datetime', 'null'=>true]
         ]);
         
-        $this->forge->addForeignKey('koleksi_id', 'koleksi', 'id', 'cascade', 'set null');
-        $this->forge->addForeignKey('anggota_id', 'anggota', 'id', 'cascade', 'set null');
-        $this->forge->addForeignKey('pustakawan_id', 'pustakawan', 'id', 'cascade', 'set null');
+        $this->forge->addForeignKey('koleksi_id', 'koleksi', 'id', 'cascade');
+        $this->forge->addForeignKey('anggota_id', 'anggota', 'id', 'cascade');
+        $this->forge->addForeignKey('pustakawan_id', 'pustakawan', 'id', 'cascade');
         $this->forge->createTable('stok_koleksi');
     }
 
