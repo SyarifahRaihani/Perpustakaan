73b79501755588c9b086cbeb2ce198fd28b63d35
Minanda-12210368 <113584641+Minanda-12210368@users.noreply.github.com>
2022-10-20 12:59:39 +0700

    StokKoleksiSeeder

---
+++ app/Database/Seeds/StokKoleksiSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use App\Models\KoleksiModel;
+use CodeIgniter\Database\Seeder;
+
+class StokKoleksiSeeder extends Seeder
+{
+    public function run()
+    {
+        $r = (int)(new KoleksiModel())->insert([
+            'koleksi_id'        => '10',
+            'nomor'             => '10',
+            'status_tersedia'   => 'A',
+            #'anggota_id'       => ,
+            #'pustakawan_id'    => ,
+            
+
+        ]);
+        echo "hasil insert $r";
+    }
+}
